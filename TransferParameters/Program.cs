436e93902ed9294895de1f1bf95cbd11c0f96589
Book <Book@WIN-8CS3AUJICSU>
2023-10-12 22:54:21 +0300

    encoding

---
+++ TransferParameters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void Exchange(int a, int b)
+        {
+                int buffer = a;
+                a = b;
+                b = buffer;
+        }
+    }
+}
